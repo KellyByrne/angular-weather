2db3fb6c83fa5fa24ef5671bbc5eababbd94331f
Kelly Byrne <kellyannbyrne@utexas.edu>
2016-01-27 22:06:01 -0600

    added current weather to title

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -6,19 +6,20 @@ app.controller("WeatherCtrl", ['$scope', '$http', '$location', 'City', function(
 
 		console.log('Location:' + city + "," + state)
 
-		$http.get('http://api.wunderground.com/api/62db039518025c86/forecast10day/q/' + state + '/' + city + '.json')
-			.then(function(data){
-				var forecastData = data.data.forecast.simpleforecast;
-				var locationData = city + ', ' + state;
-				City.weatherData.push({location: locationData, forecast: forecastData})
-				// console.log(City.weatherData)
-				$location.path('/forecast')
-		})
+	$http.get('http://api.wunderground.com/api/62db039518025c86/forecast10day/q/' + state + '/' + city + '.json')
+		.then(function(data){
+			var forecastData = data.data.forecast.simpleforecast;
+			var locationData = city + ', ' + state;
+			City.weatherData.push({location: locationData, forecast: forecastData})
+			// console.log(City.weatherData)
+			$location.path('/forecast')
+	})
 	}
 }])
 
 app.controller("ForecastCtrl", ['$scope', '$location', 'City', function($scope, $location, City){
 	$scope.place = City.weatherData[0].location;
+	$scope.currentWeather = City.weatherData[0].forecast.forecastday[0];
 	$scope.forecasts = City.weatherData[0].forecast.forecastday.slice(1,4);
 	console.log($scope.forecasts)
 
